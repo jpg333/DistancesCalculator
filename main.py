8d7b41ae3b09fec94ffe14fbf1081155fafa7684
jpg333 <joshpgriffith@gmail.com>
2020-09-30 21:09:20 -0400

    commit

--- main.py
+++ main.py
@@ -1,64 +1,83 @@
 import math
 import os
 import random
-
-import openpyxl
-import xlsxwriter
-
-
-# initialize excel file
-wbWrite = xlsxwriter.Workbook("Distances.xlsx")
-sheetWrite = wbWrite.add_worksheet()
-
-wbRead = openpyxl.load_workbook(filename='Distances.xlsx', data_only=True)
-sheetRead = wbRead.get_sheet_by_name("Sheet1")
-
-columns = ["X1", "Y1", "X2", "Y2"]          # list for column names
-
-pointCols = 4                               # number of columns that the coordinate data points' information exists in
-pointCount = 20                             # variable for number of data points
+import xlrd
+import xlwt
 
 
 def writeCols(cols):
-   for j in range(len(cols)):
-       sheetWrite.write(0, j, cols[j])
+    for j in range(len(cols)):
+        sheetW.write(0, j, cols[j])
+    writebook.save("Distances.xls")
 
 
 # populates excel file with random values for data points
 def randomPoints():
-   for i in range(1, pointCount):
-       for j in range(len(columns)):
-           sheetWrite.write(i, j, random.randint(1, 100))
+    for i in range(1, pointCount):
+        for j in range(len(columns)):
+            sheetW.write(i, j, random.randint(1, 100))
+    writebook.save("Distances.xls")
 
 
 # calculate and write Euclidean distances for coordinates
 def eucDist():
-   # list for the calculated euc distances
-   eucDistances = []
 
-   for i in range(1, pointCount):
-       coords = []
-       for j in range(pointCols):
-           cell = sheetRead.cell(i+1, j+1)
-           coords.append(cell.value)
-       print(str(coords))
-       x1 = coords[0]
-       y1 = coords[1]
-       x2 = coords[2]
-       y2 = coords[3]
-       euc = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
-       eucDistances.append(euc)
+    columns.append("Euclidean Distance")
 
-   columns.append("Euclidean Distance")
-   for i in range(1, pointCount):
-       sheetWrite.write(i, columns.index("Euclidean Distance"), eucDistances[i-1])
+    for i in range(1, pointCount):
+        x1 = sheetR.cell_value(i, columns.index("X1"))
+        y1 = sheetR.cell_value(i, columns.index("Y1"))
+        x2 = sheetR.cell_value(i, columns.index("X2"))
+        y2 = sheetR.cell_value(i, columns.index("Y2"))
+
+        euc = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
+        sheetW.write(i, columns.index("Euclidean Distance"), euc)
+        sheetW.col(columns.index("Euclidean Distance")).width = 4500
+
+    writebook.save("Distances.xls")
 
 
+# calculate and write Manhattan distances for coordinates
+def manhattanDist():
+    columns.append("Manhattan Distance")
 
+    for i in range(1, pointCount):
+        x1 = sheetR.cell_value(i, columns.index("X1"))
+        y1 = sheetR.cell_value(i, columns.index("Y1"))
+        x2 = sheetR.cell_value(i, columns.index("X2"))
+        y2 = sheetR.cell_value(i, columns.index("Y2"))
+
+        man = abs(x2 - x1) + abs(y2 - y1)
+        sheetW.write(i, columns.index("Manhattan Distance"), man)
+        sheetW.col(columns.index("Manhattan Distance")).width = 4500
+
+    writebook.save("Distances.xls")
+
+
+# initialize writing to workbook with xlwt
+writebook = xlwt.Workbook()
+sheetW = writebook.add_sheet("Sheet1", True)
+
+# global variables
+columns = ["X1", "Y1", "X2", "Y2"]      # list for column names
+
+pointCols = 4                           # number of columns that the coordinate data points' information exists in
+pointCount = 20                            # variable for number of data points
+
+# write current column names
 writeCols(columns)
 randomPoints()
-eucDist()
-writeCols(columns)
 
-wbWrite.close()
+# initialize reading from workbook with xlrd
+#   (done separately from xlwt initialization so that workbook is written to and saved with random points first,
+#   then opened to be read from
+path = os.path.expanduser("Distances.xls")
+readbook = xlrd.open_workbook(path)
+sheetR = readbook.sheet_by_index(0)
+
+eucDist()
+manhattanDist()
+
+# rewrite current column names (after the edition of the euclidean distance column
+writeCols(columns)
 
